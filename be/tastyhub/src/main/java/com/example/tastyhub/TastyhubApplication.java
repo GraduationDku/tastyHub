da0d92ca11c519cf46011b4aab814ea9c9126366
skyriv213 <>
2024-03-28 16:36:31 +0900

    feat : tasthub backend init

---
+++ be/tastyhub/src/main/java/com/example/tastyhub/TastyhubApplication.java
@@ -0,0 +1,13 @@
+package com.example.tastyhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastyhubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TastyhubApplication.class, args);
+    }
+
+}
