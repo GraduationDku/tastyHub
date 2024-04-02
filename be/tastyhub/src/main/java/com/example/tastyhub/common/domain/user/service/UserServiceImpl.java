9bbccb299aaa499a08d0e2c5d1316da4258a2ae2
skyriv213 <>
2024-04-02 18:09:29 +0900

    feat(<User>) : username, nickname 중복체크 로직
    username,nickname 중복 체크 단위 테스트 및 실행 로직 작성 - TDD
    
    #6

--- be/tastyhub/src/main/java/com/example/tastyhub/common/domain/user/service/UserServiceImpl.java
+++ be/tastyhub/src/main/java/com/example/tastyhub/common/domain/user/service/UserServiceImpl.java
@@ -1,5 +1,52 @@
 package com.example.tastyhub.common.domain.user.service;
 
-public class UserServiceImpl {
+import com.example.tastyhub.common.domain.user.dtos.DuplicatedNickName;
+import com.example.tastyhub.common.domain.user.dtos.DuplicatedUserName;
+import com.example.tastyhub.common.domain.user.dtos.LoginRequest;
+import com.example.tastyhub.common.domain.user.dtos.SignupRequest;
+import com.example.tastyhub.common.domain.user.repository.UserRepository;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+//    private final BCryptPasswordEncoder passwordEncoder;
+
+
+    @Override
+    @Transactional
+    public void checkDuplicatedUsername(DuplicatedUserName duplicatedUserName) {
+        if (!userRepository.existsByUsername(duplicatedUserName.getUsername())) {
+            return;
+        }
+        throw new IllegalArgumentException("이미 존재하는 username입니다");
+    }
+
+    @Override
+    @Transactional
+    public void checkDuplicatedNickname(DuplicatedNickName duplicatedNickName) {
+        if (!userRepository.existsByNickname(duplicatedNickName.getNickname())) {
+            return;
+        }
+        throw new IllegalArgumentException("이미 존재하는 nickname입니다");
+    }
+
+    @Override
+    public void signup(SignupRequest signupRequest) {
+
+    }
+
+    @Override
+    public void login(LoginRequest loginRequest, HttpServletResponse response) {
+
+    }
+
 
 }
--- be/tastyhub/src/test/java/com/example/tastyhub/common/domain/user/service/UserServiceImplTest.java
+++ be/tastyhub/src/test/java/com/example/tastyhub/common/domain/user/service/UserServiceImplTest.java
@@ -1,8 +1,18 @@
 package com.example.tastyhub.common.domain.user.service;
 
+import static com.example.tastyhub.fixture.user.UserFixture.DUPLICATED_NICK_NAME;
+import static com.example.tastyhub.fixture.user.UserFixture.DUPLICATED_USER_NAME;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import com.example.tastyhub.common.domain.user.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,35 +29,55 @@ class UserServiceImplTest {
     UserRepository userRepository;
 
     @InjectMocks
-    UserService userService;
+    UserServiceImpl userService;
 
     @Test
     @DisplayName("username 중복 x")
     void checkDuplicatedUsername() {
-
+        when(userRepository.existsByUsername(any())).thenReturn(false);
+        userService.checkDuplicatedUsername(DUPLICATED_USER_NAME);
+        verify(userRepository, times(1)).existsByUsername(any());
     }
 
     @Test
     @DisplayName("username 중복 o")
     void failCheckDuplicatedUserName() {
+        when(userRepository.existsByUsername(any())).thenReturn(true);
 
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            userService.checkDuplicatedUsername(DUPLICATED_USER_NAME);
+        });
+
+        assertEquals("이미 존재하는 username입니다", exception.getMessage());
+        verify(userRepository, times(1)).existsByUsername(any());
     }
 
     @Test
     @DisplayName("nickname 중복 x")
     void checkDuplicatedNickname() {
+        when(userRepository.existsByNickname(any())).thenReturn(false);
+        userService.checkDuplicatedNickname(DUPLICATED_NICK_NAME);
+        verify(userRepository, times(1)).existsByNickname(any());
 
     }
 
     @Test
     @DisplayName("nickname 중복 o")
     void failCheckDuplicatedNickname() {
+        when(userRepository.existsByNickname(any())).thenReturn(true);
 
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            userService.checkDuplicatedNickname(DUPLICATED_NICK_NAME);
+        });
+
+        assertEquals("이미 존재하는 nickname입니다", exception.getMessage());
+        verify(userRepository, times(1)).existsByNickname(any());
     }
 
     @Test
     @DisplayName("signup 성공")
     void signup() {
+
     }
 
     @Test
