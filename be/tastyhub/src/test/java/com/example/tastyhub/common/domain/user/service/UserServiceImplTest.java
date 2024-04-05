9faec09ca297b65d2398dc87431f0752ce0d36ee
skyriv213 <kimminsoo213@dankook.ac.kr>
2024-04-05 21:56:37 +0900

    test(<user>) : login 단위 테스트 작성
    resolve : #6

--- be/tastyhub/src/test/java/com/example/tastyhub/common/domain/user/service/UserServiceImplTest.java
+++ be/tastyhub/src/test/java/com/example/tastyhub/common/domain/user/service/UserServiceImplTest.java
@@ -2,6 +2,7 @@ package com.example.tastyhub.common.domain.user.service;
 
 import static com.example.tastyhub.fixture.user.UserFixture.DUPLICATED_NICK_NAME;
 import static com.example.tastyhub.fixture.user.UserFixture.DUPLICATED_USER_NAME;
+import static com.example.tastyhub.fixture.user.UserFixture.LOGIN_REQUEST;
 import static com.example.tastyhub.fixture.user.UserFixture.SIGNUP_REQUEST;
 import static com.example.tastyhub.fixture.user.UserFixture.USER;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,11 +10,15 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.example.tastyhub.common.domain.user.repository.UserRepository;
+import com.example.tastyhub.common.utils.Jwt.JwtUtill;
+import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -32,6 +37,9 @@ class UserServiceImplTest {
     UserRepository userRepository;
     @Mock
     PasswordEncoder passwordEncoder;
+
+    @Mock
+    JwtUtill jwtUtill;
 
     @InjectMocks
     UserServiceImpl userService;
@@ -89,10 +97,29 @@ class UserServiceImplTest {
     @Test
     @DisplayName("login 성공")
     void login() {
+        HttpServletResponse response = mock(HttpServletResponse.class);
+
+        given(passwordEncoder.matches(any(), any())).willReturn(true);
+        given(userRepository.findByUsername(any())).willReturn(USER);
+        userService.login(LOGIN_REQUEST, response);
+        verify(userRepository, times(1)).findByUsername(LOGIN_REQUEST.getUsername());
+        verify(jwtUtill, times(1)).createAccessToken(any(), any());
+        verify(jwtUtill, times(1)).createRefreshToken(any(), any());
+
     }
 
     @Test
     @DisplayName("login 실패")
     void failLogin() {
+        HttpServletResponse response = mock(HttpServletResponse.class);
+
+        given(userRepository.findByUsername(any())).willReturn(USER);
+        given(passwordEncoder.matches(any(), any())).willReturn(false);
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            userService.login(LOGIN_REQUEST, response);
+        });
+
+        assertEquals("비밀번호가 일치하지않습니다.", exception.getMessage());
     }
 }
