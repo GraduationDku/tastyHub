098021bc7a39ab4451b12459f8ed05b84a9c523b
sep037 <tmddms0307@dankook.ac.kr>
2024-04-08 01:21:37 +0900

    feat(<user>): 어떤 화면을 띄워줄지 결정하는 함수 작성
    서버랑 아직 제대로 안 해봐서 그런지 화면 이동이 안 먹음
    
    #4

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import HomeScreen from './components/HomeScreen';
+import Login from './components/Login';
+import Signup from './components/Signup';
 
 function App() {
+  const [screen, setScreen] = useState('home'); // 'home', 'login', 'signup'
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {screen === 'home' && <HomeScreen setScreen={setScreen} />}
+      {screen === 'login' && <Login setScreen={setScreen} />}
+      {screen === 'signup' && <Signup setScreen={setScreen} />}
     </div>
   );
 }
+
 
 export default App;
