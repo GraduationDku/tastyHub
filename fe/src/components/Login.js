c7cfcf84bd56cf8fcab3447facc4ebe7e23945c4
sep037 <tmddms0307@dankook.ac.kr>
2024-04-08 01:23:16 +0900

    feat(<user>) : 로그인 구현
    #4

---
+++ fe/src/components/Login.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+
+function Login({ setScreen }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('/user/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username,
+          password,
+        }),
+      });
+
+      if (response.ok) {
+        const authorization = response.headers.get('Authorization');
+        const refreshToken = response.headers.get('Refresh');
+        localStorage.setItem('accessToken', authorization);
+        localStorage.setItem('refreshToken', refreshToken);
+        console.log('로그인 성공');
+      } else {
+        console.error('로그인 실패');
+      }
+    } catch (error) {
+      console.error('로그인 중 오류 발생:', error);
+    }
+  };
+  return (
+    <div>
+      <h2>Login</h2>
+      <input
+        type="text"
+        placeholder="ID"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="PASSWORD"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>로그인</button>
+      <button onClick={() => setScreen('signup')}>Sign Up</button>
+    </div>
+  );
+  };
+
+  
+
+
+export default Login;
