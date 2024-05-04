0d26675ff6b9c15221823c3be10746591980f63b
skyriv213 <kimminsoo213@dankook.ac.kr>
2024-05-04 19:00:36 +0900

    TEST <(Village)> : 지역정보 단위 테스트 진행
    지역정보 단위테스트 진행 - 지역정보 수정 하기 및 Fixture 생성
    
    #30

--- be/tastyhub/src/test/java/com/example/tastyhub/fixture/village/VillageFixture.java
+++ be/tastyhub/src/test/java/com/example/tastyhub/fixture/village/VillageFixture.java
@@ -1,6 +1,10 @@
 package com.example.tastyhub.fixture.village;
 
+import static com.example.tastyhub.fixture.user.UserFixture.USER;
+
+import com.example.tastyhub.common.domain.user.entity.User;
 import com.example.tastyhub.common.domain.village.dtos.LocationRequest;
+import com.example.tastyhub.common.domain.village.entity.Village;
 
 public class VillageFixture {
 
@@ -9,6 +13,13 @@ public class VillageFixture {
         .lat((long) 37.395628)
         .lng((long) 126.929901)
         .addressTownName("탄현동")
+        .build();
+
+    public static Village VILLAGE = Village.builder()
+        .addressTownName("동네이름")
+        .user(USER)
+        .lng(1L)
+        .lat(1L)
         .build();
 
 }
