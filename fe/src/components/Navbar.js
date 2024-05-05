01c4a10e88907d39cc9967265fec3a46e55d37b4
sep037 <tmddms0307@dankook.ac.kr>
2024-05-05 20:50:19 +0900

    feat <(nav)> : 내비게이션 화면 전환 경로 설정
    #8

--- fe/src/components/Navbar.js
+++ fe/src/components/Navbar.js
@@ -5,8 +5,8 @@ import '../css/Navbar.css';
 const Navbar = ({ setScreen }) => {
   return (
     <div className="navbar">
-      <button onClick={() => setScreen('')}>TastyHub</button>
-      <button onClick={() => setScreen('')}>레시피</button>
+      <button onClick={() => setScreen('home')}>TastyHub</button>
+      <button onClick={() => setScreen('recipe')}>레시피</button>
       <button onClick={() => setScreen('')}>재료 공유</button>
       <button onClick={() => setScreen('')}>채팅</button>
       <button onClick={() => setScreen('')}>마이페이지</button>
