c27a73ce7d1db9c0ab34aa93e39fe42adaa7e4e1
skyriv213 <kimminsoo213@dankook.ac.kr>
2024-05-06 22:40:17 +0900

    Test <(Comment)> :  댓글 단위 테스트 진행
    댓글 단위 테스트 진행 : 댓글 생성, 수정, 삭제 성공/실패 단위테스트 진행
    
    #21

--- be/tastyhub/src/main/java/com/example/tastyhub/common/domain/comment/service/CommentServiceImpl.java
+++ be/tastyhub/src/main/java/com/example/tastyhub/common/domain/comment/service/CommentServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.tastyhub.common.domain.comment.service;
 
+import lombok.Generated;
 import org.springframework.stereotype.Service;
 
 import com.example.tastyhub.common.domain.comment.dtos.CommentRequest;
@@ -19,21 +20,33 @@ public class CommentServiceImpl implements CommentService{
 
     @Override
     public void createComment(Long postId, CommentRequest commentCreateRequest, User user) {
-        Post post = postRepository.findById(postId).get();
+        Post post = getPost(postId);
         Comment comment = Comment.builder().post(post).text(commentCreateRequest.getText()).user(user).isAlive(true).build();
         commentRepository.save(comment);
     }
 
     @Override
     public void updateComment(Long commentId, CommentRequest commentUpdateRequest, User user) {
-        Comment comment = commentRepository.findById(commentId).get();
+        Comment comment = findById(commentId);
         comment.update(commentUpdateRequest);
     }
 
     @Override
     public void deleteComment(Long commentId, User user) {
-        Comment comment = commentRepository.findById(commentId).get();
+        Comment comment = findById(commentId);
         comment.delete();
+    }
+
+    @Generated
+    private Comment findById(Long commentId) {
+        return commentRepository.findById(commentId)
+            .orElseThrow(() -> new IllegalArgumentException("해당 댓글은 존재하지 않습니다"));
+    }
+
+    @Generated
+    private Post getPost(Long postId) {
+        return postRepository.findById(postId)
+            .orElseThrow(() -> new IllegalArgumentException("해당 게시글은 존재하지 않습니다."));
     }
 
 }
--- be/tastyhub/src/test/java/com/example/tastyhub/common/domain/comment/service/CommentServiceImplTest.java
+++ be/tastyhub/src/test/java/com/example/tastyhub/common/domain/comment/service/CommentServiceImplTest.java
@@ -1,8 +1,22 @@
 package com.example.tastyhub.common.domain.comment.service;
 
+import static com.example.tastyhub.fixture.comment.CommentFixture.COMMENT;
+import static com.example.tastyhub.fixture.comment.CommentFixture.COMMENT_REQUEST;
+import static com.example.tastyhub.fixture.post.PostFixture.POST;
+import static com.example.tastyhub.fixture.user.UserFixture.USER;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import com.example.tastyhub.common.domain.comment.repository.CommentRepository;
+import com.example.tastyhub.common.domain.post.repository.PostRepository;
+import com.example.tastyhub.common.domain.user.entity.User;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,8 +26,12 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class CommentServiceImplTest {
+
     @Mock
     CommentRepository commentRepository;
+
+    @Mock
+    PostRepository postRepository;
 
     @InjectMocks
     CommentServiceImpl commentService;
@@ -21,30 +39,58 @@ class CommentServiceImplTest {
     @Test
     @DisplayName("댓글 생성")
     void createComment() {
+        given(postRepository.findById(any())).willReturn(Optional.ofNullable(POST));
+        commentService.createComment(POST.getId(), COMMENT_REQUEST, USER);
+        verify(commentRepository, times(1)).save(any());
     }
 
     @Test
     @DisplayName("댓글 수정")
     void updateComment() {
+        given(commentRepository.findById(any())).willReturn(Optional.ofNullable(COMMENT));
+        commentService.updateComment(POST.getId(), COMMENT_REQUEST, USER);
+        verify(commentRepository, times(1)).findById(any());
     }
 
     @Test
     @DisplayName("댓글 삭제")
     void deleteComment() {
+        given(commentRepository.findById(any())).willReturn(Optional.ofNullable(COMMENT));
+        commentService.deleteComment(POST.getId(), USER);
+        verify(commentRepository, times(1)).findById(any());
     }
 
     @Test
     @DisplayName("댓글 생성 실패")
     void createCommentFail() {
+        given(postRepository.findById(any())).willThrow(
+            new IllegalArgumentException("해당 게시글은 존재하지 않습니다."));
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            commentService.createComment(POST.getId(), COMMENT_REQUEST, USER);
+        });
+        assertEquals("해당 게시글은 존재하지 않습니다.",exception.getMessage());
+
     }
 
     @Test
     @DisplayName("댓글 수정 실패")
     void updateCommentFail() {
+        given(commentRepository.findById(any())).willThrow(
+            new IllegalArgumentException("해당 댓글은 존재하지 않습니다."));
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            commentService.updateComment(POST.getId(), COMMENT_REQUEST, USER);
+        });
+        assertEquals("해당 댓글은 존재하지 않습니다.",exception.getMessage());
     }
 
     @Test
     @DisplayName("댓글 삭제 실패")
     void deleteCommentFail() {
+        given(commentRepository.findById(any())).willThrow(
+            new IllegalArgumentException("해당 댓글은 존재하지 않습니다."));
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            commentService.deleteComment(POST.getId(), USER);
+        });
+        assertEquals("해당 댓글은 존재하지 않습니다.",exception.getMessage());
     }
 }
