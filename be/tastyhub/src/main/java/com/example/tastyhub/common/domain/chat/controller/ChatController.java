393b200a11301f99c609c1bd143bb0116bd35d39
skyriv213 <kimminsoo213@dankook.ac.kr>
2024-05-15 18:15:37 +0900

    feat <(chat)> : 채팅 기능 로직 구현
    프론트 연결 후 테스트 필요
    
    #23

--- be/tastyhub/src/main/java/com/example/tastyhub/common/domain/chat/controller/ChatController.java
+++ be/tastyhub/src/main/java/com/example/tastyhub/common/domain/chat/controller/ChatController.java
@@ -1,7 +1,12 @@
 package com.example.tastyhub.common.domain.chat.controller;
 
+import com.example.tastyhub.common.domain.chat.dtos.ChatDto;
 import com.example.tastyhub.common.domain.chat.service.ChatService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChatController {
 
     private final ChatService chatService;
+
+
+    @MessageMapping("/chat/rooms/{roodId}/send")
+    @SendTo("/topic/public/rooms/{roomId}")
+    public ChatDto sendMessage(@PathVariable Long roomId, @Payload ChatDto chatMessage) {
+
+        return chatService.createChat(roomId, chatMessage);
+    }
 }
--- be/tastyhub/src/main/java/com/example/tastyhub/common/domain/chat/service/ChatService.java
+++ be/tastyhub/src/main/java/com/example/tastyhub/common/domain/chat/service/ChatService.java
@@ -1,5 +1,8 @@
 package com.example.tastyhub.common.domain.chat.service;
 
+import com.example.tastyhub.common.domain.chat.dtos.ChatDto;
+
 public interface ChatService {
 
+    ChatDto createChat(Long roomId, ChatDto chatMessage);
 }
