55da4a3b0a57a8d6e860eb1293bd9477e320e109
Chelly142 <67005998+Chelly142@users.noreply.github.com>
2024-05-28 12:27:01 +0900

    feat <(userReview)> :  UserReview 기능 추가를 위한 선작업 #26

--- be/tastyhub/src/main/java/com/example/tastyhub/common/domain/userReview/controller/UserReviewController.java
+++ be/tastyhub/src/main/java/com/example/tastyhub/common/domain/userReview/controller/UserReviewController.java
@@ -1,5 +1,18 @@
 package com.example.tastyhub.common.domain.userReview.controller;
 
-public class UserReviewController {
+import static com.example.tastyhub.common.config.APIConfig.USERREVIEW_API;
+import static com.example.tastyhub.common.utils.HttpResponseEntity.RESPONSE_OK;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.tastyhub.common.domain.userReview.service.UserReviewService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping(USERREVIEW_API)
+@RequiredArgsConstructor
+public class UserReviewController {
+    private final UserReviewService userReviewService;
 }
--- be/tastyhub/src/main/java/com/example/tastyhub/common/domain/userReview/service/UserReviewServiceImpl.java
+++ be/tastyhub/src/main/java/com/example/tastyhub/common/domain/userReview/service/UserReviewServiceImpl.java
@@ -1,5 +1,13 @@
 package com.example.tastyhub.common.domain.userReview.service;
 
-public class UserReviewServiceImpl implements UserReviewService{
+import org.springframework.stereotype.Service;
 
+import com.example.tastyhub.common.domain.userReview.repository.UserReviewRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserReviewServiceImpl implements UserReviewService{
+    private final UserReviewRepository userReviewRepository;
 }
