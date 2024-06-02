994a2be4887a295eed53c4f80c605404073539a2
Chelly142 <67005998+Chelly142@users.noreply.github.com>
2024-06-02 16:24:05 +0900

    test <(scrap)> : 스크랩 단위 테스트 작성 #26

---
+++ be/tastyhub/src/test/java/com/example/tastyhub/common/domain/scrap/service/ScrapServiceImplTest.java
@@ -0,0 +1,53 @@
+package com.example.tastyhub.common.domain.scrap.service;
+
+import static com.example.tastyhub.fixture.recipe.RecipeFixture.RECIPE;
+import static com.example.tastyhub.fixture.user.UserFixture.USER;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.example.tastyhub.common.domain.scrap.repository.ScrapRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class ScrapServiceImplTest {
+    @Mock
+    ScrapRepository scrapRepository;
+
+    @InjectMocks
+    ScrapServiceImpl scrapService;
+
+    @Test
+    @DisplayName("스크랩이 존재할 때 삭제")
+    void likeAlreadyExists() {
+        when(scrapRepository.existsByRecipeIdAndUserId(USER.getId(), RECIPE.getId())).thenReturn(true);
+
+        // when
+        scrapService.scrap(RECIPE.getId(),USER);
+
+        // then
+        verify(scrapRepository, times(1)).deleteByRecipeIdAndUserId(any(), any());
+        verify(scrapRepository, times(0)).save(any());
+    }
+
+    @Test
+    @DisplayName("스크랩이 존재하지 않을 때 생성")
+    void likeDoesNotExist() {
+       
+        when(scrapRepository.existsByRecipeIdAndUserId(USER.getId(), RECIPE.getId())).thenReturn(false);
+
+        // when
+        scrapService.scrap(RECIPE.getId(),USER);
+
+        // then
+        verify(scrapRepository, times(0)).deleteByRecipeIdAndUserId(any(), any());
+        verify(scrapRepository, times(1)).save(any());
+    }
+}
