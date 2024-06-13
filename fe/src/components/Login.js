db716fc363f03d7728c06300993c091ed8b05f70
sep037 <tmddms0307@dankook.ac.kr>
2024-06-13 12:26:26 +0900

    fix <(bug)> : 로그인 토큰 값 재설정

--- fe/src/components/Login.js
+++ fe/src/components/Login.js
@@ -28,6 +28,7 @@ function Login({ setScreen }) {
         localStorage.setItem('accessToken', authorization);
         localStorage.setItem('refreshToken', refreshToken);
         localStorage.setItem('nickname', nickname);
+        
   
         console.log('로그인 성공');
         setScreen('main');
