2ade4bec0c774fd825469a5f436312fe426f01f8
skyriv213 <kimminsoo213@dankook.ac.kr>
2024-08-28 20:36:15 +0900

    fix <(cors)> : cors 허용 재설정
    #123

--- be/tastyhub/src/main/java/com/example/tastyhub/common/config/SecurityConfig.java
+++ be/tastyhub/src/main/java/com/example/tastyhub/common/config/SecurityConfig.java
@@ -54,7 +54,8 @@ public class SecurityConfig {
         "http://localhost:8080",
         "https://apic.app",
         "http://localhost:3000",
-        "http://13.209.238.65"
+        "http://13.209.238.65",
+        "https://localhost:3000"
     };
 
 
