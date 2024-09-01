d3ae070e2a4d8c90e8d5771631e13aab74a981a1
sep037 <tmddms0307@dankook.ac.kr>
2024-09-02 01:55:30 +0900

    fix <(Like)> : reducer 변경
    #119

--- fe/src/store/store.js
+++ fe/src/store/store.js
@@ -1,6 +1,9 @@
 // state들 보관하는 파일
 import { configureStore } from '@reduxjs/toolkit'
+import likeReducer from './redux/Like/likeState';
 
 export default configureStore({
-	reducer: {}
-})
+	reducer: {
+		like : likeReducer
+	}
+});
