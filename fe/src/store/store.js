d091bb9e3bb95496e6efbacd470cc5445defd93f
sep037 <tmddms0307@dankook.ac.kr>
2024-09-03 13:24:12 +0900

    fix <(Signup)> : 회원가입 리듀서 생성, 컴포넌트 수정
    #119

--- fe/src/store/store.js
+++ fe/src/store/store.js
@@ -2,10 +2,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import likeReducer from '../../src/redux/Like/likeState.js';
 import loginReducer from '../../src/redux/User/loginState.js';
+import signupReducer from '../../src/redux/User/signupState.js';
 
 export default configureStore({
 	reducer: {
 		like : likeReducer,
 		login : loginReducer,
+		signup : signupReducer,
 	}
 });
