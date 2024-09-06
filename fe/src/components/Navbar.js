14152c0bde112514d3e4905cfb16f729ab9265b7
sep037 <tmddms0307@dankook.ac.kr>
2024-09-07 03:24:03 +0900

    Refactory <(User)> : 사용자 조회 로직 생성
    #119

--- fe/src/components/Navbar.js
+++ fe/src/components/Navbar.js
@@ -4,6 +4,7 @@ import '../css/Navbar.css';
 
 const Navbar = ({ setScreen, onSearchComplete }) => {
   const [searchTerm, setSearchTerm] = useState('');
+  const [isUserSearch, setIsUserSearch] = useState(false);
 
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
@@ -11,7 +12,11 @@ const Navbar = ({ setScreen, onSearchComplete }) => {
 
   const handleSearchSubmit = async (event) => {
     event.preventDefault();
-    await fetchRecipes();
+    if (isUserSearch) {
+      await fetchUsers(searchTerm, 1, 10, 'asc');
+    } else {
+      await fetchRecipes();
+    }
   };
 
   const fetchRecipes = async () => {
@@ -25,31 +30,59 @@ const Navbar = ({ setScreen, onSearchComplete }) => {
       });
       if (response.ok) {
         const recipe = await response.json();
-        onSearchComplete(recipe.content);  // Trigger search complete action
+        onSearchComplete(recipe.content);
       } else {
-        throw new Error('Failed to fetch recipes');
+        throw new Error('레시피를 가져오는 데 실패했습니다.');
       }
     } catch (error) {
-      console.error('Error fetching recipes:', error);
+      console.error('레시피 가져오기 오류:', error);
+    }
+  };
+
+  const fetchUsers = async (nickname, page, size, sort) => {
+    const url = `${process.env.REACT_APP_API_URL}/user/search-list?nickname=${nickname}&page=${page}&size=${size}&sort=${sort}`;
+    try {
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      if (response.ok) {
+        const data = await response.json();
+        onSearchComplete(data.userDtoList);
+      } else {
+        throw new Error('사용자 정보를 가져오는 데 실패했습니다.');
+      }
+    } catch (error) {
+      console.error('사용자 정보 가져오기 오류:', error);
     }
   };
 
   return (
     <div className="navbar">
       <button className='main' onClick={() => setScreen('main')}>TastyHub</button>
-        <button onClick={() => setScreen('recipe')}>레시피</button>
-        <button onClick={() => setScreen('post')}>재료 공유</button>
-        <button onClick={() => setScreen('mainchat')}>채팅</button>
-        <button onClick={() => setScreen('mypage')}>마이페이지</button>
+      <button onClick={() => setScreen('recipe')}>레시피</button>
+      <button onClick={() => setScreen('post')}>재료 공유</button>
+      <button onClick={() => setScreen('mainchat')}>채팅</button>
+      <button onClick={() => setScreen('mypage')}>마이페이지</button>
 
       <form className="search-form" onSubmit={handleSearchSubmit}>
         <input
           type="text"
-          placeholder="            레시피를 검색하세요 !"
+          placeholder={isUserSearch ? "사용자를 검색하세요!" : "레시피를 검색하세요!"}
           value={searchTerm}
           onChange={handleSearchChange}
           className="search-input"
         />
+        <button
+          type="button"
+          onClick={() => setIsUserSearch(!isUserSearch)} // 검색 모드 토글
+          className="search-toggle-btn"
+        >
+          {isUserSearch ? "레시피 검색" : "사용자 검색"}
+        </button>
+        <button type="submit">검색</button>
       </form>
     </div>
   );
