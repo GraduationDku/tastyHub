fd00c5da6ad4c976a711420332a7657f1d99993c
skyriv213 <80723571+skyriv213@users.noreply.github.com>
2024-09-27 14:05:15 +0900

    feature <(cache)> : 캐시 전략 수립 및 캐시 구현을 위한 compose 및 config 세팅
    Co-authored-by:  Chelly142 <67005998+Chelly142@users.noreply.github.com>
    
    #129

--- be/tastyhub/src/main/java/com/example/tastyhub/common/config/RedisConfig.java
+++ be/tastyhub/src/main/java/com/example/tastyhub/common/config/RedisConfig.java
@@ -1,6 +1,5 @@
 package com.example.tastyhub.common.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,36 +7,56 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableRedisRepositories
 public class RedisConfig {
 
-    @Value("${spring.data.redis.host}")
-    private String host;
+    @Value("${spring.data.redis1.host}")
+    private String verifiedHost;
 
-    @Value("${spring.data.redis.port}")
-    private int port;
+    @Value("${spring.data.redis1.port}")
+    private int verifiedPort;
+
+    @Value("${spring.data.redis2.host}")
+    private String cacheHost;
+
+    @Value("${spring.data.redis2.port}")
+    private int cachePort;
 
 //    @Value("${spring.data.redis.password}")
 //    private String password;
 
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
+    public RedisConnectionFactory redisConnectionFactoryForVerified() {
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-        redisStandaloneConfiguration.setHostName(host);
-        redisStandaloneConfiguration.setPort(port);
+        redisStandaloneConfiguration.setHostName(verifiedHost);
+        redisStandaloneConfiguration.setPort(verifiedPort);
 //        redisStandaloneConfiguration.setPassword(password);
         LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
         return lettuceConnectionFactory;
     }
     @Bean
-    public RedisTemplate<?, ?> redisTemplate() {
+    public RedisTemplate<?, ?> redisTemplateForVerified() {
         RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setConnectionFactory(redisConnectionFactoryForVerified());
+        return redisTemplate;
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactoryForCache() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(cacheHost);
+        redisStandaloneConfiguration.setPort(cachePort);
+//        redisStandaloneConfiguration.setPassword(password);
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
+        return lettuceConnectionFactory;
+    }
+    @Bean
+    public RedisTemplate<?, ?> redisTemplateForCache() {
+        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactoryForCache());
         return redisTemplate;
     }
 
