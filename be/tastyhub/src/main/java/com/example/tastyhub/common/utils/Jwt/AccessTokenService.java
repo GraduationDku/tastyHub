0150454ca11d1c649aa6a7f99902229a7b66848b
Chelly142 <tpgus0510@naver.com>
2024-10-30 21:57:37 +0900

    feature <(token)> : 프론트 개발을 위한 access 토큰 만료시간 10초로 지정

--- be/tastyhub/src/main/java/com/example/tastyhub/common/utils/Jwt/AccessTokenService.java
+++ be/tastyhub/src/main/java/com/example/tastyhub/common/utils/Jwt/AccessTokenService.java
@@ -30,7 +30,7 @@ public class AccessTokenService {
   public static final String REFRESH_HEADER = "Refresh";
   public static final String AUTHORIZATION_KEY = "auth";
   private static final String BEARER_PREFIX = "Bearer ";
-  private static final long ACCESS_TOKEN_TIME = 60 * 60 * 1000L;
+  private static final long ACCESS_TOKEN_TIME = 10 * 1000L;
 
   //    @Value("${jwt.secret.key}")
 
