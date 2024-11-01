acc9109418efbe7ccf6d81574f0ce5a4c15641ac
skyriv213 <80723571+skyriv213@users.noreply.github.com>
2024-11-01 09:37:41 +0900

    fix ,feature <(OAuth)> : OAuth
    프론트에서 사용자 값은 넘어오는데 백에서 구글 서버 진행과정 중 400 발생
    
    #154

--- be/tastyhub/src/main/java/com/example/tastyhub/common/utils/oauth/service/OAuth2Service.java
+++ be/tastyhub/src/main/java/com/example/tastyhub/common/utils/oauth/service/OAuth2Service.java
@@ -3,8 +3,15 @@ package com.example.tastyhub.common.utils.oauth.service;
 import com.example.tastyhub.common.domain.user.dtos.NicknameDto;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.Map;
+import org.springframework.http.ResponseEntity;
 
 public interface OAuth2Service {
 
   NicknameDto login(Map<String, Object> attributes, HttpServletResponse response);
+
+  ResponseEntity<?> accessToken(String code);
+
+//  void getUserInfo(String accessToken, HttpServletResponse response);
+
+  ResponseEntity<?> getUserInfo(String accessToken);
 }
