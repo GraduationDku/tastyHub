560e032ef672fbd4307c2c0b1be8fb478f559bb5
sep037 <tmddms0307@dankook.ac.kr>
2024-11-04 14:47:28 +0900

    feat <(video)> : 주석 삭제
    #158

--- fe/src/components/GoogleLoginButton.js
+++ fe/src/components/GoogleLoginButton.js
@@ -9,7 +9,6 @@ function GoogleLoginButton() {
   const handleLoginSuccess = async (credentialResponse) => {
     const tokenId = credentialResponse.credential;
     console.log(tokenId)
-    // Optionally, send tokenId to the server for validation/authentication
     const response = await fetch(`https://localhost/oauth2/authorization/google?tokenId=${tokenId}`, {
       method: "POST",
       headers: {
