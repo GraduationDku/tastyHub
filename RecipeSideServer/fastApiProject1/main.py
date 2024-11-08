135080c09726ff696ef69156c0077169d94680fc
skyriv213 <80723571+skyriv213@users.noreply.github.com>
2024-11-09 00:04:31 +0900

    feature <(video)> : 유튜브 영상 요약 GPT 호출
    API 연결 및 호출 확인 완료
    
    #158
    Co-authored-by: Chelly142 <67005998+Chelly142@users.noreply.github.com>

--- RecipeSideServer/fastApiProject1/main.py
+++ RecipeSideServer/fastApiProject1/main.py
@@ -1,18 +1,21 @@
+import os
+
 import uvicorn
 from fastapi import FastAPI, File, UploadFile
 import boto3
 from botocore.exceptions import NoCredentialsError
 from intoGPT import create_prediction
 from video.useVideoServiceAction import processingVideo,temp
+from video.useYoutubeServiceAction import youtubeAnalysis
 
 
 app = FastAPI()
 
 # AWS S3 설정 (Bucket 이름, AWS 인증 정보)
-S3_BUCKET = "your-s3-bucket-name"
-S3_ACCESS_KEY = "your-access-key"
-S3_SECRET_KEY = "your-secret-key"
-S3_REGION = "your-region"
+S3_BUCKET = os.getenv("S3_BUCKET")
+S3_ACCESS_KEY = os.getenv("S3_ACCESS_KEY")
+S3_SECRET_KEY = os.getenv("S3_SECRET_KEY")
+S3_REGION = os.getenv("S3_REGION")
 
 
 # S3에 파일 업로드 함수
@@ -41,11 +44,25 @@ async def say_hello(foodName: str, step: str):
 
     return {"message": f"Hello {temp_cookStep}"}
 
+# 유튜브 레시피 분석 엔드포인트
+@app.post("/video/youtube/link")
+async def upload_video(youtubeUrl: str):
+
+    # 영상 처리 로직 진행
+    response_body = youtubeAnalysis(youtubeUrl)
+    return response_body
+
+@app.post("/youtube/test")
+async def upload_video():
+    link = "https://www.youtube.com/watch?v=pLiO21Hhb-U"
+    # 영상 처리 로직 진행
+    response_body = youtubeAnalysis(link)
+    return response_body
 
 # 영상만 타임라인 받는 엔드포인트
 @app.post("/recipe/video/timeline")
 async def upload_video(steps: str, foodName: str, video: UploadFile = File(...)):
-    # gpt 프로프트
+    # gpt 프롬프트
     temp_cookStep = create_prediction(foodName,steps)
 
     # 영상 처리 로직 진행
