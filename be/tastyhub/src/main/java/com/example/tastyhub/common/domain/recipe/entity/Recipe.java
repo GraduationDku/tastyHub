cd1cce4cb8a1733f19ab4a167013648ba09bc07f
Chelly142 <67005998+Chelly142@users.noreply.github.com>
2024-11-14 12:55:41 +0900

    feat (recipe) : Recipe에 recipeType enum 필드 추가

--- be/tastyhub/src/main/java/com/example/tastyhub/common/domain/recipe/entity/Recipe.java
+++ be/tastyhub/src/main/java/com/example/tastyhub/common/domain/recipe/entity/Recipe.java
@@ -12,6 +12,8 @@ import com.example.tastyhub.common.domain.user.entity.User;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -51,6 +53,10 @@ public class Recipe extends TimeStamped {
 
     @Column(name = "food_img",length = 1024)
     private String recipeMediaFileUrl;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private RecipeType recipeType;
 
 
 //    private Long numLike;
