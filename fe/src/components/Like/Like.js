330f370e08a5a0170a6aed1eb348feb5814d2057
sep037 <tmddms0307@dankook.ac.kr>
2024-11-20 02:34:03 +0900

    feat <(icons)> : 레시피 상세보기에 좋아요, 스크랩 아이콘 설정 및 라이브러리 설치
    #173

--- fe/src/components/Like/Like.js
+++ fe/src/components/Like/Like.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchLikeCount, toggleLike } from '../../redux/Like/likeState';
+import { FaThumbsUp, FaRegThumbsUp } from 'react-icons/fa'; // React Icons
 import '../../css/RecipeDetail.css';
 
 const LikeButton = ({ recipeId }) => {
@@ -20,8 +21,27 @@ const LikeButton = ({ recipeId }) => {
 
   return (
     <div>
-      <button className='recipedetail' onClick={handleToggleLike}>
-        {liked ? 'Unlike' : 'Like'} ({likeCount})
+      <button
+        className='recipedetail'
+        onClick={handleToggleLike}
+        style={{
+          background: 'none',
+          border: 'none',
+          cursor: 'pointer',
+          display: 'flex',
+          alignItems: 'center',
+          gap: '8px',
+          color: liked ? '#3eab5c' : '#787878',
+          fontSize: '16px',
+          transition: 'transform 0.3s',
+          marginLeft:'40px',
+          marginRight:'-35px',
+
+        }}
+        onMouseOver={(e) => (e.currentTarget.style.transform = 'scale(1.1)')}
+        onMouseOut={(e) => (e.currentTarget.style.transform = 'scale(1)')}
+      >
+        {liked ? <FaThumbsUp /> : <FaRegThumbsUp />}{likeCount}
       </button>
     </div>
   );
--- fe/src/components/Recipe/Recipedetails.js
+++ fe/src/components/Recipe/Recipedetails.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect, useRef } from 'react';
-import CreateRecipeReview from '../Review/CreateRecipeReview';
+import ReactStars from 'react-rating-stars-component';
 import LikeButton from '../Like/Like';
 import ScrapButton from '../Scrap/Scrap';
+import StarRatings from 'react-star-ratings';
 import YouTube from 'react-youtube'; // YouTube 라이브러리
 import '../../css/RecipeDetail.css';
 
 function RecipeDetails({ recipeId }) {
   const [recipeDetails, setRecipeDetails] = useState(null);
   const [recipeReviews, setRecipeReviews] = useState([]);
+  const [rating, setRating] = useState(0); // 별점 상태
+  const [reviewContent, setReviewContent] = useState(''); // 리뷰 내용 상태
   const videoRef = useRef(null); // 비디오 컨트롤을 위한 ref
   const youtubePlayerRef = useRef(null); // YouTube 플레이어 ref
 
@@ -58,7 +61,30 @@ function RecipeDetails({ recipeId }) {
     }
   }, [recipeId]);
 
-  // HH:MM:SS를 초 단위로 변환하는 함수
+  const handleReviewSubmit = async () => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/recipe-review/create/${recipeId}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': localStorage.getItem('accessToken'),
+        },
+        body: JSON.stringify({ grade: rating, content: reviewContent }),
+      });
+  
+      if (response.ok) {
+        const newReview = { nickname: '사용자', grade: rating, content: reviewContent }; // 새 리뷰 데이터
+        setRecipeReviews((prev) => [...prev, newReview]); // 리뷰 리스트 업데이트
+        setRating(0); // 별점 초기화
+        setReviewContent(''); // 리뷰 내용 초기화
+      } else {
+        console.error('Failed to submit review:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error submitting review:', error);
+    }
+  };
+
   const convertToSeconds = (timeString) => {
     const timeParts = timeString.split(':').map(Number); // HH:MM:SS -> [HH, MM, SS]
     return timeParts.reduce((acc, val) => acc * 60 + val, 0); // 초 단위로 변환
@@ -96,21 +122,50 @@ function RecipeDetails({ recipeId }) {
 
   return (
     <div className="recipedetails">
-      <h1>{recipeDetails.foodName}</h1>
+      <br /><br />
       <div className="box">
+        <h1
+          style={{
+            fontSize: '24px',
+            marginBottom: '16px',
+            textAlign: 'center',
+            color: '#3eab5c',
+          }}
+        >
+          {recipeDetails.foodName}
+        </h1>
         {/* 대표 사진 */}
-        <img src={recipeDetails.foodImgUrl} alt={recipeDetails.foodName} />
+        <img
+          src={recipeDetails.foodImgUrl}
+          alt={recipeDetails.foodName}
+          style={{
+            width: '100%',
+            height: 'auto',
+            transition: 'transform 0.3s',
+          }}
+          onMouseOver={(e) => (e.currentTarget.style.transform = 'scale(1.05)')}
+          onMouseOut={(e) => (e.currentTarget.style.transform = 'scale(1)')}
+        />
 
         {/* 요리 설명 */}
-        <div>
+        <div
+          style={{
+            backgroundColor: '#f9f9f9',
+            borderRadius: '8px',
+            paddingBottom: '40px',
+            marginBottom: '16px',
+            boxShadow: '0 2px 6px rgba(0, 0, 0, 0.1)',
+          }}
+        >
+          <br />
           <p className="dis">
-            <strong>레시피 설명<br /><br /></strong> {recipeDetails.foodInformation.content}
+            <strong style={{ color: '#3eab5c' }}>레시피 설명<br /><br /></strong> {recipeDetails.foodInformation.content}
           </p>
           <p className="time">
-            <strong>조리 시간 </strong> {recipeDetails.foodInformation.cookingTime}분
+            <strong style={{ color: '#3eab5c' }}>조리 시간 <br /><br /></strong> {recipeDetails.foodInformation.cookingTime}분
           </p>
           <p className="amount">
-            <strong>양 </strong> {recipeDetails.foodInformation.serving}
+            {recipeDetails.foodInformation.serving}<strong style={{ color: '#3eab5c' }}>&nbsp;인분</strong>
           </p>
         </div>
 
@@ -127,15 +182,6 @@ function RecipeDetails({ recipeId }) {
         </div>
 
         {/* 순서 */}
-        {/* 비디오 */}
-        {/* {recipeDetails.recipeType === 'Video' && recipeDetails.foodVideoUrl && (
-          <video
-            ref={videoRef}
-            src={'https://tastyhub-bucket.s3.ap-northeast-2.amazonaws.com/videos/sample2.mp4'}
-            controls
-            style={{ width: '100%', maxHeight: '500px' }}
-          />
-)} */}
         {recipeDetails.recipeType === 'Video' && (
           <video
             ref={videoRef}
@@ -166,7 +212,6 @@ function RecipeDetails({ recipeId }) {
                 onClick={() => handleTimelineClick(step.timeLine)}
               >
                 {step.content}
-
                 {/* 단계별 이미지 */}
                 {recipeDetails.recipeType === 'photo' && step.cookStepImg && (
                   <img
@@ -185,24 +230,85 @@ function RecipeDetails({ recipeId }) {
           <LikeButton className="recipedetail" recipeId={recipeId} />
           <ScrapButton recipeId={recipeId} />
         </p>
+        <hr />
+        {/* 리뷰 섹션 */}
+        <div className="review" style={{ marginTop: '40px' }}>
+          <h3 style={{ marginBottom: '20px', fontSize: '18px', fontWeight: 'bold', color: '#3eab5c' }}>
+            리뷰
+          </h3>
+          <ul style={{ listStyleType: 'none', padding: '0', marginBottom: '20px' }}>
+            {recipeReviews.map((review, index) => (
+              <li
+                key={index}
+                style={{
+                  marginBottom: '20px',
+                  borderBottom: '1px solid #ddd',
+                  paddingBottom: '10px',
+                }}
+              >
+                <p style={{ marginBottom: '8px', fontWeight: 'bold' }}></p>
+                <p style={{ marginBottom: '8px', color: '#ffd700' }}>
+                  {Array.from({ length: 5 }, (_, i) => (
+                    <span key={i} style={{ fontSize: '20px' }}>
+                      {i < review.grade ? '★' : '☆'}
+                    </span>
+                  ))}
+                </p>
+                <p style={{ fontSize: '14px', color: '#555' }}>{review.content}</p>
+              </li>
+            ))}
+          </ul>
+          
+          {/* 별점 및 리뷰 작성 */}
+          <div
+            style={{
+              display: "flex", // 가로 배치를 위한 flex 컨테이너
+              flexDirection: "row", // 가로 방향으로 배치
+              justifyContent: "flex-start", // 왼쪽 정렬
+              alignItems: "center", // 세로 중앙 정렬
+              gap: "15px", // 별과 텍스트 영역 사이 간격
+              width: "100%", // 컨테이너의 너비
+            }}
+          >
+            <ReactStars
+              count={5}
+              value={rating} // 상태를 반영
+              key={rating} // 강제로 재렌더링을 유도하기 위해 key 추가
+              onChange={(newRating) => setRating(newRating)}
+              size={20}
+              activeColor="#ffd700"
+            />
+            {/* 텍스트 영역 */}
+            <textarea
+              placeholder="리뷰를 작성하세요"
+              value={reviewContent}
+              onChange={(e) => setReviewContent(e.target.value)}
+              style={{
+                width: "100%", // 텍스트 박스의 너비를 100%로 설정
+                maxWidth: "400px", // 최대 너비 제한
+                padding: "10px",
+                border: "1px solid #ddd",
+                borderRadius: "5px",
+                resize: "none", // 사용자가 크기를 조정할 수 없도록 설정
+                boxSizing: "border-box", // 패딩과 보더를 포함한 전체 크기를 맞춤
+              }}
+            ></textarea>
+            <button
+            onClick={handleReviewSubmit}
+            style={{
+              backgroundColor: '#3EAB5C',
+              color: 'white',
+              border: 'none',
+              cursor: 'pointer',
+              width:'80px',
+              marginBottom :'20px'
+            }}>
+            제출
+          </button>
+          </div>
 
-        {/* 리뷰 */}
-        <div className="review">
-          <div>
-            <ul>
-              {recipeReviews.map((review, index) => (
-                <li key={index}>
-                  <p>
-                    <strong>{review.nickname}</strong> (평점 : {review.grade})
-                  </p>
-                  <p>{review.content}</p>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div>
-            <CreateRecipeReview recipeId={recipeId} />
-          </div>
+
+          
         </div>
       </div>
     </div>
